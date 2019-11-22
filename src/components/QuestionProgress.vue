44598aae26d8c56bcec0c00c7523afe0100e722c
Margaret Tavares <m.tavares@vehikl.com>
2019-11-21 21:03:21 -0500

    Cleaned up components

--- src/components/QuestionProgress.vue
+++ src/components/QuestionProgress.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="trivia-question">
         <div>Score Progress: </div>
-
     </div>
 </template>
 
@@ -24,42 +23,5 @@ export default class QuestionProgress extends Vue {
         display: flex;
         flex-direction: column;
         padding: 1rem;
-
-        .question-prompt {
-            font-size: 1.5rem;
-            color: hsl(0, 0%, 21%);
-            text-align: center;
-        }
-        .answers-container {
-            margin-top: 2rem;
-            .possible-answer {
-                background-color: hsl(210, 80%, 46%);
-                color: whitesmoke;
-                min-width: 25rem;
-                padding: 0.75rem;
-                border-radius: 0.25rem;
-                &:not(:first-child) {
-                    margin-top: 0.5rem;
-                }
-                font-size: 1rem;
-
-                &:hover {
-                    cursor: pointer;
-                    filter: brightness(120%);
-                }
-
-                &.correct {
-                    background-color: forestgreen;
-                }
-
-                &.incorrect {
-                    background-color: orangered;
-                }
-            }
-
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
     }
 </style>
